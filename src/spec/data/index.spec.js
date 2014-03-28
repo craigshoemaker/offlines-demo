66df0db76b61ab293af8adf1bf37d35d897d48bf
Craig Shoemaker <craig@craigshoemaker.net>
2014-03-28 06:35:02 -0700

    Updated Jasmine tests to support asycn

--- src/spec/data/index.spec.js
+++ src/spec/data/index.spec.js
@@ -2,40 +2,46 @@
 
 describe('/data/index.js: ', function(){
 
-    it('"getParks" will return all parks (without projections)', function(){
-        db.parks.get(function(parks){
-            expect(parks.length).toBeGreaterThan(0);   
+    it('"getParks" will return all parks (without projections)', function(done){
+        db.parks.get(function(error, parks){
+            expect(parks.length).toBeGreaterThan(0);
+            done();
         });
     });
 
-    it('"getParkByName" will return only one park matching the given name', function(){
+    it('"getParkByName" will return only one park matching the given name', function(done){
         var parkName = 'EPCOT'
-        db.parks.getByName(parkName, function(park){
-            expect(park.name).toBe(parkName);    
+        db.parks.getByName(parkName, function(error, park){
+            expect(park.name).toBe(parkName);
+            done();
         });
     });
 
-    it('"createPark" will create a new park with the given name', function(){
-        db.parks.create('test', function(error){
+    it('"createPark" will create a new park with the given name', function(done){
+        var parkName = 'test';
+        db.parks.create(parkName, function(error){
             expect(error).toBeNull();
+            done();
         });    
     });
 
-    it('"createPark" will NOT create duplicates', function(){
-        db.parks.create('test', function(error){
+    it('"createPark" will NOT create duplicates', function(done){
+        var parkName = 'test';
+
+        db.parks.create(parkName, function(error){
             expect(error).toBeTruthy();
+            done();
         });    
     });
 
-    it('"deletePark" will delete a park with the given name', function(){
+    it('"deletePark" will delete a park with the given name', function(done){
         var parkName = 'test'
-
         db.parks.delete(parkName, function(error){
             expect(error).toBeNull();
+            db.parks.getByName(parkName, function(park){
+                expect(park).toBeNull();
+                done(); 
+            });
         }); 
-        
-        db.parks.getByName(parkName, function(park){
-            expect(park).toBeNull();    
-        });
     });
 });
