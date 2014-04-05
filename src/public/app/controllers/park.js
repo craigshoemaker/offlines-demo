0ab4b5c6e77b04f448fda2493421f01011959909
Craig Shoemaker <craig@craigshoemaker.net>
2014-04-05 07:06:37 -0700

    Updated park view to use ng-options

--- src/public/app/controllers/park.js
+++ src/public/app/controllers/park.js
@@ -10,16 +10,29 @@ angular.module('offlines').controller('parkController',
 
             var parkName = parts[parts.length-1];
 
+            var buildDurationOptions = function (){
+                var arr = [];
+
+                for(var i=0;i<121;i+=5){
+                    arr.push({ duration: i});   
+                }
+
+                arr.push({duration: '120+'});
+
+                return arr;
+            };
+
+            $scope.durationOptions = buildDurationOptions();
+
             $scope.park = $http.get('/api/parks/' + parkName).success(function(park){
                 $scope.park = park;
             });
-
 
             $scope.back = function(){
                 $window.history.back();
             };
 
-            $scope.save = function(e){
+            $scope.save = function(){
                 $window.alert('save wait times');
             };
 
