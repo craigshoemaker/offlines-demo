774c5d9d4d332c8a2271e0f8b6bdf924d9d1c0ff
Craig Shoemaker <craig@craigshoemaker.net>
2014-04-21 15:21:23 -0700

    Added no cache header to appcache file

--- src/controllers/ui/indexController.js
+++ src/controllers/ui/indexController.js
@@ -13,6 +13,7 @@
 
         app.get('/app.appcache',function(request, response){
             response.set('Content-Type', 'text/cache-manifest');
+            response.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
             response.render('appcache');
         });
     };
